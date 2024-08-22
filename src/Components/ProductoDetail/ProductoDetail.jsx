30aa324626503de16955d8f8457f8400e566b832
Ezequiel <ezequielgiribarren@gmail.com>
2024-08-21 21:41:57 -0300

    modificaciones

--- src/Components/ProductoDetail/ProductoDetail.jsx
+++ src/Components/ProductoDetail/ProductoDetail.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { productos } from '../../productos';
 import Slider from 'react-slick'; // Importa el componente Slider
-import Layout from '../../Layout/Layout'
-
+import Layout from '../../Layout/Layout';
+import Contacto from '../Contacto/Contacto';
 
 const ProductoDetaill = () => {
   const { title } = useParams();
@@ -18,19 +18,23 @@ const ProductoDetaill = () => {
   }
 
   // Arreglo de las imágenes del producto
-  const images = [producto.img1, producto.img2, producto.img3, producto.img4, producto.img4, producto.img4];
+  const images = [producto.img1, producto.img2, producto.img3, producto.img4];
   const modelos = producto.modelos || {};
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: false,
     speed: 500,
     slidesToShow: 2.5,  // Muestra dos imágenes completas y media
     slidesToScroll: 1,
     centerPadding: '150', // Ajusta el padding para mostrar la media imagen desde la derecha,
-    arrows: 'true'
+    arrows: true
   };
 
+  // Desplazar la página hacia arriba cuando se monta el componente
+  useEffect(() => {
+    window.scrollTo(0, 0);  // Mueve el scroll al inicio de la página al cargar el componente
+  }, []);
 
   // Actualiza el estado del scroll
   useEffect(() => {
@@ -59,7 +63,7 @@ const ProductoDetaill = () => {
               </div>
             ))}
           </div>
-          <div className="description-horizontal" style={{ transform: `translateY(${animated ? Math.min(scrollY / -0.4, 100) : 0}px)` }}>
+          <div className="description-horizontal" style={{ transform: `translateY(${animated ? Math.min(scrollY / -0.6, 100) : 0}px)` }}>
             <div className="description-horizontal-description">
               <div className='description-slogan'>
                 <h3>La más alta calidad</h3>
@@ -79,43 +83,39 @@ const ProductoDetaill = () => {
                   </button>
                 </div>
               </div>
-            </div><div className="container-fluid">
-              <div className="row modelo-1">
-                <h3>{producto.modelos['modelo-1'].nombre}</h3>
-                <Slider {...settings} className='slider-model'>
-                  {modelos["modelo-1"] && Object.values(modelos["modelo-1"]).slice(1).map((img, index) => (
-                    <div key={index} className='item-slider'>
-                      <img src={img} alt={`Modelo 1 - Imagen ${index + 1}`} className="img-fluid" />
-                    </div>
-                  ))}
-                </Slider>
-                <div className='row modelo-description'>
-                  <div className="col-md-6 modelo-title">
-                    <h3 className='h3-designe' dangerouslySetInnerHTML={{ __html: producto.modelos['modelo-1'].nombre2 }}></h3>
-                  </div>
-                  <div className="col-md-6">
-                    <p className='p-modelo' dangerouslySetInnerHTML={{ __html: producto.modelos['modelo-1'].descripcion }}></p>
-                  </div>
-                </div>
-              </div>
+            </div>
 
-              {/* Slider para modelo-2 */}
-              <div className="row modelo-2">
-                <h3>{producto.modelos['modelo-2'].nombre}</h3>
-                <Slider {...settings} className='slider-model'>
-                  {modelos["modelo-2"] && Object.values(modelos["modelo-2"]).slice(1).map((img, index) => (
-                    <div key={index} className='item-slider'>
-                      <img src={img} alt={`Modelo 2 - Imagen ${index + 1}`} className="img-fluid" />
+            <div className="container-fluid">
+              {/* Itera sobre los modelos y renderiza un componente para cada uno */}
+              {Object.keys(modelos).map((modeloKey, index) => {
+                const modelo = modelos[modeloKey];
+                return (
+                  <div className={`row modelo-${index + 1}`} key={modeloKey}>
+                    <h3>{modelo.nombre}</h3>
+                    <Slider {...settings} className='slider-model'>
+                      {Object.values(modelo.imagenes).map((img, imgIndex) => (
+                        <div key={imgIndex} className='item-slider'>
+                          <img src={img} alt={`Modelo ${index + 1} - Imagen ${imgIndex + 1}`} className="img-fluid" />
+                        </div>
+                      ))}
+                    </Slider>
+                    <div className='row modelo-description'>
+                      <div className="col-md-6 modelo-title">
+                        <h3 className='h3-designe' dangerouslySetInnerHTML={{ __html: modelo.nombre2 }}></h3>
+                      </div>
+                      <div className="col-md-6 modelo-p">
+                        <p className='p-modelo' dangerouslySetInnerHTML={{ __html: modelo.descripcion }}></p>
+                      </div>
                     </div>
-                  ))}
-                </Slider>
-              </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </section>
+      <Contacto />
     </Layout>
-
   );
 };
 
--- src/productos.js
+++ src/productos.js
@@ -13,18 +13,24 @@ export const productos = [
       "modelo-1": {
         nombre: "Modelo Alux",
         nombre2: "Modelo<br />Alux",
+        imagenes: {
         img1: 'images/productos/cerramientos/alux/1.png',
         img2: 'images/productos/cerramientos/alux/2.png',
         img3: 'images/productos/cerramientos/alux/1.png',
         img4: 'images/productos/cerramientos/alux/2.png',
+        },
         descripcion: "Este modelo consta de un perfil de aluminio extruido, anodizado en color gris y fijado mediante anclaje químico al piso refractario de la pileta, cubriendo el perímetro de la misma. Se completa con vidrios templados o laminados de 10 mm que ofrecen seguridad, al mismo tiempo que no interrumpen la vista, dando al ambiente la naturalidad soñada.  Las puertas incluyen bisagras en acero inoxidable y el cierre puede ser mediante pasador o cerradura con llave. Todo en acero inoxidable."
       },
       "modelo-2": {
         nombre: "Modelo Espiga",
+        nombre2: "Modelo<br />Espiga",
+        imagenes: {
         img1: 'images/productos/cerramientos/espiga/1.png',
         img2: 'images/productos/cerramientos/espiga/2.png',
         img3: 'images/productos/cerramientos/espiga/1.png',
         img4: 'images/productos/cerramientos/espiga/2.png',
+        },
+
         descripcion: "Este modelo consta de soportes en forma de espiga confeccionados en acero inoxidable 304 con terminación pulido esmerilado. Se fijan al piso refractario de la pileta mediante varillas de acero inoxidable con fijación química. Están diseñadas para colocar vidrios templados de 10 a 15 mm que ofrecen seguridad y no interrumpen la vista dando al ambiente una naturalidad inmejorable. <br />Las puertas incluyen bisagras en acero inoxidable y el cierre puede ser mediante pasador o cerradura con llave. Todo en acero inoxidable."
       }
     }
@@ -33,18 +39,54 @@ export const productos = [
     id: 2,
     imagen: 'images/productos/2.png',
     titulo: 'Barandas para escaleras',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Contamos con 3 modelos que pueden adaptarse perfectamente a cualquier ambientación otorgando estilo y seguridad a tu hogar.',
     slug: 'barandas-para-escaleras',
     img1: 'images/productos/barandas/1.png',
     img2: 'images/productos/barandas/2.png',
     img3: 'images/productos/barandas/3.png',
-    img4: 'images/productos/barandas/4.png'
+    img4: 'images/productos/barandas/4.png',
+    modelos: {
+      "modelo-1": {
+        nombre: "Modelo Berlín",
+        nombre2: "Modelo<br />Berlín",
+        imagenes: {
+        img1: 'images/productos/cerramientos/alux/1.png',
+        img2: 'images/productos/cerramientos/alux/2.png',
+        img3: 'images/productos/cerramientos/alux/1.png',
+        img4: 'images/productos/cerramientos/alux/2.png',
+        },
+        descripcion: "Este modelo está íntegramente realizado en acero inoxidable 304, pulido esmerilado.Pueden optar entre dos presentaciones: Una con columnas de caño redondo de 50 mm, con roseta y embellecedor, y pasamanos redondo. La segunda opción es de columnas de caño cuadradas de 40 mm, con roseta y embellecedor, y pasamanos rectangular. En ambos casos se utilizan 4 barras o caños de contención de 13 mm."
+      },
+      "modelo-2": {
+        nombre: "Modelo Vienna",
+        nombre2: "Modelo<br />Vienna",
+        imagenes: {
+        img1: 'images/productos/cerramientos/espiga/1.png',
+        img2: 'images/productos/cerramientos/espiga/2.png',
+        img3: 'images/productos/cerramientos/espiga/1.png',
+        img4: 'images/productos/cerramientos/espiga/2.png',
+        },
+
+        descripcion: "Este modelo combina una estructura de acero inoxidable 304 con vidrio templado de 10mm. Pueden optar entre dos presentaciones: Una con columnas de caño redondo de 50 mm, con roseta y embellecedor, y pasamanos redondo. La segunda opción es de columnas de caño cuadradas de 40 mm, con roseta y embellecedor, y pasamanos rectangular. En ambos casos se utiliza vidrio templado, en la zona de contención, sujeto por clips de acero inoxidable a las columnas de la escalera."
+      },
+      "modelo-3": {
+        nombre: "Modelo Florencia",
+        nombre2: "Modelo<br />Florencia",
+        imagenes: {
+        img1: 'images/productos/cerramientos/espiga/1.png',
+        img2: 'images/productos/cerramientos/espiga/2.png',
+        img3: 'images/productos/cerramientos/espiga/1.png',
+        img4: 'images/productos/cerramientos/espiga/2.png',
+        },
+        descripcion: "Este modelo está íntegramente realizado en vidrio templado de 10 mm que se fija a la escalera mediante sujetadores redondos de acero inoxidable. Opcional: pasamano sobre el vidrio."
+      }
+    }
   },
   {
     id: 3,
     imagen: 'images/productos/3.png',
     titulo: 'Pasamanos para interiores y exteriores',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Diseñados en caño de acero inoxidable 304, pulido esmerilado, de 38 mm y 50 mm de diámetro. ideales para escaleras internas, entradas a edificios y accesos de todo tipo. Están fijados con soportes y tornillería de acero inoxidable lo que brinda una calidad, estilo y durabilidad inmejorables para un producto que lo acompañará de por vida.',
     slug: 'pasamanos-para-interiores-y-exteriores',
     img1: 'images/productos/pasamanos/1.png',
     img2: 'images/productos/pasamanos/2.png',
@@ -55,7 +97,7 @@ export const productos = [
     id: 4,
     imagen: 'images/productos/4.png',
     titulo: 'Rejillas de desagüe para duchas',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Lo último en diseño para desagües de ducha, baños y lavaderos. Nuestros modelos están íntegramente realizados en acero inoxidable 304 pulido esmerilado de 1mm de espesor que se integran de manera perfecta al piso del espacio que elijas. Contamos con modelos con terminación en acero inoxidable, con diseños que suman detalles de estilo único a sus ambientes, y otros que pueden incluir terminación en el mismo material de sus pisos para unificar la vista del espacio. El sistema de nuestros desagües permite drenar una mayor cantidad de agua impidiendo el retorno de la misma y son muy fáciles de limpiar. Apto para silla de ruedas',
     slug: 'rejillas-de-desague-para-duchas',
     img1: 'images/productos/rejillas-duchas/1.png',
     img2: 'images/productos/rejillas-duchas/2.png',
@@ -66,7 +108,7 @@ export const productos = [
     id: 5,
     imagen: 'images/productos/5.png',
     titulo: 'Revestimiento de acero inoxidable',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Revestimientos íntegramente confeccionados en chapa de acero inoxidable 430 y 304. La opción ideal para cocinas, gastronomía y restaurantes ya que le permitirá crear un estilo único y distintivo con un producto de alta calidad, resistente, duradero y muy fácil de limpiar. Contamos con distintos acabados que ofrecen la posibilidad de darle un toque único a su espacio.',
     slug: 'revestimiento-de-acero-inoxidable',
     img1: 'images/productos/revestimiento/1.png',
     img2: 'images/productos/revestimiento/2.png',
@@ -77,7 +119,7 @@ export const productos = [
     id: 6,
     imagen: 'images/productos/6.png',
     titulo: 'Chapas decorativas',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'La opción perfecta para dar un toque único a puertas, portones, balcones, biombos, separadores o generar puntos focales de decoración en su patio o jardín. Confeccionadas en acero inoxidable 304, pulido esmerilado y mate, ofrecen un estilo único a sus espacios interiores y exteriores con una duración de por vida ya que son resistentes a la intemperie.',
     slug: 'chapas-decorativas',
     img1: 'images/productos/chapas/1.png',
     img2: 'images/productos/chapas2.png',
@@ -88,7 +130,7 @@ export const productos = [
     id: 7,
     imagen: 'images/productos/7.png',
     titulo: 'Zócalos en acero inoxidable',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Los zócalos de acero inoxidable aportan un detalle único a tus espacios al mismo tiempo que ofrecenestilo, resistencia y durabilidad.  Contamos con dos presentaciones: rectangulares y borde sanitario. Los rectangulares, de acero inoxidable 430 y 304 pulido esmerilado, son la opción perfecta para oficinas, casas, empresas y locales que buscan dar un rasgo distintivo y diferente a sus espacios. Los de borde sanitario están confeccionados en acero inoxidable 304 y 316, pulido esmerilado. Esta opción es ideal para sanatorios, hospitales, laboratorios y farmacias que requieren una resistencia especial a todo tipo de químicos utilizados tanto en pisos como mesadas de trabajo. Un beneficio extra: Ambos son muy fáciles de limpiar y mantener.',
     slug: 'zocalos-en-acero-inoxidable',
     img1: 'images/productos/zocalos/1.png',
     img2: 'images/productos/zocalos/2.png',
@@ -99,7 +141,7 @@ export const productos = [
     id: 8,
     imagen: 'images/productos/8.png',
     titulo: 'Rejillas de desagüe pluvial',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Ideales para el drenaje de jardines, piletas, patios y galerías dándole un estilo único a sus espacios exteriores. Cuentan con un marco y una rejilla de acero inoxidable 304, pulido esmerilado, en diferentes medidas. Este material asegura calidad y durabilidad de por vida. Apto tránsito vehicular.',
     slug: 'rejillas-de-desague-pluvial',
     img1: 'images/productos/rejillas-pluvial/1.png',
     img2: 'images/productos/rejillas-pluvial/2.png',
@@ -110,8 +152,8 @@ export const productos = [
     id: 9,
     imagen: 'images/productos/9.png',
     titulo: 'Rejillas de ventilación',
-    descripcion: 'Descripción del Servicio 1',
-    slug: 'rejillas-de-ventilacion',
+    descripcion: 'Nuestros modelos de rejilla están íntegramente confeccionados en acero inoxidable 304, pulido esmerilado, lo que asegura una durabilidad de por vida que se suma a la elegancia, estilo y diseño de sus ambientes.Contamos con distintas medidas estándar y también con medidas y diseños especiales a pedido',//● Rejilla de ventilación fija – Retorno / Extracción (acero inox esmerilado)o 10x10 (muebles)o 15x15o 20x20o 25x25o 30x10o 30x15o 30x30o 40x10o 40x20,
+    slug: 'rejillas-de-ventilacion' ,
     img1: 'images/productos/rejillas-ventilacion/1.png',
     img2: 'images/productos/rejillas-ventilacion/2.png',
     img3: 'images/productos/rejillas-ventilacion/3.png',
@@ -121,7 +163,7 @@ export const productos = [
     id: 10,
     imagen: 'images/productos/10.png',
     titulo: 'Pasamanos y barandas doble homologado',
-    descripcion: 'Descripción del Servicio 1',
+    descripcion: 'Diseños adaptables a cualquier espacio, que cumplen con las normas del gobierno de la ciudad, sin dejar de dar el estilo buscado. La opción perfecta para los accesos a sanatorios, edificios, locales, instituciones y bancos.',
     slug: 'pasamanos-y-barandas-doble-homologado',
     img1: 'images/productos/pasamanos-doble/1.png',
     img2: 'images/productos/pasamanos-doble/2.png',
